e35d18b23464d0de2c06d7b0bcf6fb9c45c9e21d
PRIYANKA182005 <145959419+PRIYANKA182005@users.noreply.github.com>
2023-09-25 11:28:01 +0530

    Add files via upload

---
+++ challenger 3.2.py
@@ -0,0 +1,16 @@
+class Student:
+  def __init__(self,name,roll_number,cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+    
+def sort_students(student_list):
+  sorted_students = sorted(student_list,key=lambda student:student.cgpa,reverse=True)
+  return sorted_students
+  
+students = [Student("hari","A123",7.6),Student("Srikanth","A124",8.9),Student("Saumya","A125",9.1),Student("Mahidhar","A126",9.9),]
+
+sort_students = sort_students(students)
+
+for student in sort_students:
+  print("Name: {}, Roll Number: {}, cgpa: {}".format(student.name,student.roll_number,student.cgpa))
